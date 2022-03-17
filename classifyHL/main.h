13da6e604cd61002eb491b3115722a6c34819ad7
Dave Glover <dglover@microsoft.com>
2022-03-17 15:45:46 +1100

    clang formatted

--- classifyHL/main.h
+++ classifyHL/main.h
@@ -18,11 +18,10 @@
 #include <applibs/log.h>
 #include <applibs/powermanagement.h>
 
-// clang-format off
 // https://docs.microsoft.com/en-us/azure/iot-pnp/overview-iot-plug-and-play
 #define IOT_PLUG_AND_PLAY_MODEL_ID "dtmi:com:example:azuresphere:labmonitor;1"
-#define NETWORK_INTERFACE "wlan0"
-#define SAMPLE_VERSION_NUMBER "1.0"
+#define NETWORK_INTERFACE          "wlan0"
+#define SAMPLE_VERSION_NUMBER      "1.0"
 
 #define CORE_ENVIRONMENT_COMPONENT_ID "6583cf17-d321-4d72-8283-0b7c5b56442b"
 
@@ -35,30 +34,26 @@ static void intercore_environment_receive_msg_handler(void *data_block, ssize_t 
 #define JSON_MESSAGE_BYTES 256
 static char msgBuffer[JSON_MESSAGE_BYTES] = {0};
 
-const struct itimerspec watchdogInterval = {{ 60, 0 },{ 60, 0 }};
-timer_t watchdogTimer;
-
 #define Log_Debug(f_, ...) dx_Log_Debug((f_), ##__VA_ARGS__)
 static char Log_Debug_Time_buffer[128];
 static DX_USER_CONFIG dx_config;
 
-static DX_MESSAGE_PROPERTY *messageProperties[] = {&(DX_MESSAGE_PROPERTY){.key = "appid", .value = "hvac"},
-                                                   &(DX_MESSAGE_PROPERTY){.key = "type", .value = "prediction"},
-                                                   &(DX_MESSAGE_PROPERTY){.key = "schema", .value = "1"}};
+static DX_MESSAGE_PROPERTY *messageProperties[] = {
+	&(DX_MESSAGE_PROPERTY){.key = "appid", .value = "classify"},
+	&(DX_MESSAGE_PROPERTY){.key = "type", .value = "predicton"},
+	&(DX_MESSAGE_PROPERTY){.key = "schema", .value = "1"}};
 
-static DX_MESSAGE_CONTENT_PROPERTIES contentProperties = {.contentEncoding = "utf-8", .contentType = "application/json"};
+static DX_MESSAGE_CONTENT_PROPERTIES contentProperties = {
+	.contentEncoding = "utf-8", .contentType = "application/json"};
 
-
-// declare all bindings
+// clang-format off
 static DX_DEVICE_TWIN_BINDING dt_hvac_sw_version = {.propertyName = "SoftwareVersion", .twinType = DX_DEVICE_TWIN_STRING};
 static DX_DEVICE_TWIN_BINDING dt_utc_startup = {.propertyName = "StartupUtc", .twinType = DX_DEVICE_TWIN_STRING};
 static DX_GPIO_BINDING gpio_network_led = {.pin = NETWORK_CONNECTED_LED, .name = "network_led", .direction = DX_OUTPUT, .initialState = GPIO_Value_Low, .invertPin = true};
-
 // clang-format on
 
-// All bindings referenced in the following binding sets are initialised in the InitPeripheralsAndHandlers
-// function
-DX_DEVICE_TWIN_BINDING *device_twin_bindings[] = {&dt_utc_startup, &dt_hvac_sw_version};
+// All bindings referenced are initialised in the InitPeripheralsAndHandlers function
+DX_DEVICE_TWIN_BINDING *device_twin_bindings[]         = {&dt_utc_startup, &dt_hvac_sw_version};
 DX_DIRECT_METHOD_BINDING *direct_method_binding_sets[] = {};
 DX_GPIO_BINDING *gpio_binding_sets[]                   = {&gpio_network_led};
 DX_TIMER_BINDING *timer_binding_sets[]                 = {};
@@ -71,5 +66,3 @@ DX_INTERCORE_BINDING intercore_environment_ctx = {.sockFd = -1,
 	.interCoreCallback                                    = intercore_environment_receive_msg_handler,
 	.intercore_recv_block                                 = &intercore_block,
 	.intercore_recv_block_length                          = sizeof(intercore_block)};
-
-// clang-format on
